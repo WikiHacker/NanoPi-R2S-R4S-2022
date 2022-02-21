15178c85b4aaa16efb2c06a60b0cfd79aed837f6
WikiHacker <naveed7us@gmail.com>
2022-02-21 14:44:39 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#=================================================
-# File name: hook-feeds.sh
-# Author: SuLingGG
-# Blog: https://mlapp.cn
-#=================================================
 
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
+
+# Add luci-app-eqos
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+
+# Add luci-proto-modemmanager
+svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
 
 # Add luci-app-gowebdav
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
@@ -15,18 +16,19 @@ svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/g
 # Add tmate
 git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
-#extra Packages
-git clone --depth=1 https://github.com/WikiHacker/openwrt-packages.git
-
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall luci/applications/openwrt-passwall
+# git clone -b hello https://github.com/DHDAXCW/openwrt-passwall luci/applications/openwrt-passwall
 
 # Add gotop
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
 
-
 # Add minieap
 svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
+
+# Replace smartdns with the official version
+rm -rf packages/net/smartdns
+svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
 popd
 
 # Set to local feeds
