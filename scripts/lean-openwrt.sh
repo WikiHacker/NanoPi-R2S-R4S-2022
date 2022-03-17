e3a7bd96ad03a2b7f21b017efe25d954409bde23
WikiHacker <naveed7us@gmail.com>
2022-03-17 20:57:05 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -62,7 +62,7 @@ git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../lean/luci-theme-argon
+rm -rf ../../customfeeds/luci/themes/luci-theme-argon
 
 svn co https://github.com/WikiHacker/openwrt-packages/trunk/smartdns
 svn co https://github.com/WikiHacker/openwrt-packages/trunk/luci-app-smartdns
